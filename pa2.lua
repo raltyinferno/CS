d83cdb8556bfb145f585128ec2e44dfc25733e96
Kai Davids Schell <kdavidsschell@alaska.edu>
2016-02-10 17:13:05 -0900

    added cs331 hw 2 files

---
+++ pa2.lua
@@ -0,0 +1,26 @@
+--Kai Davids Schell
+--2/5/16
+--pa2.lua
+--CS331 HW 2
+--module with concatLimit, filterTable, collatzSeq
+math = require "math"
+local pa2 = {}
+
+function pa2.concatLimit(str, leng)
+	local x = leng/str:len()
+	x,y = math.modf(x)
+	--for i
+	
+	end
+
+function pa2.filterTable(func, tab)
+
+
+	end
+	
+function pa2.collatzSeq(k)
+
+
+	end
+	
+return pa2
