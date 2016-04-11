fb3f0a45f6550f0e6b713292bc3ffc170c68472e
Scott Corcoran <scott.of.alaska@gmail.com>
2016-04-11 16:35:39 -0700

    Simpling eval down to get it to be less hard coded

--- interpit - TC.lua
+++ interpit - TC.lua
@@ -144,61 +144,61 @@ function interpit.interp(ast, state, incall, outcall)
 --                              --or ast[1] == ARRAT_REF
 --                          --... happens
 
-        if (ast[2][1][1] == UN_OP) then    ---as the AST is correct, only remaining is table
+        if (ast[1][1] == UN_OP) then    ---as the AST is correct, only remaining is table
                 print("Made it in UN_OP")
-                if ast[2][1][2] == "+" then
+                if ast[1][2] == "+" then
                     print("Made it in + thing")
-                    return strToNum(ast[2][2][2])
+                    return strToNum(ast[2][2])
                 else --this is the minus version
-                    return -(strToNum(ast[2][2][2]))
+                    return -(strToNum(ast[2][2]))
                 end
-        elseif (ast[2][1][1] == BIN_OP) then
+        elseif (ast[1][1] == BIN_OP) then
                 print("Made it in BIN_OP")
 --                val1 = eval_expr(ast[2][2][2])
 --                val2 = eval_expr(ast[2][3][2])
 --                return strToNum(val1 + val2)
-                if (ast[2][1][2] == "+") then
-                    return (strToNum(ast[2][2][2]) + strToNum(ast[2][3][2]))
-                elseif (ast[2][1][2] == "-") then
-                    return (strToNum(ast[2][2][2]) - strToNum(ast[2][3][2]))
-                elseif (ast[2][1][2] == "*") then
-                    return (strToNum(ast[2][2][2]) * strToNum(ast[2][3][2]))
-                elseif (ast[2][1][2] == "/") then
-                    return toInt((strToNum(ast[2][2][2]) / strToNum(ast[2][3][2])))
-                elseif (ast[2][1][2] == "%") then
-                    return (strToNum(ast[2][2][2]) % strToNum(ast[2][3][2]))
-                elseif (ast[2][1][2] == "==") then
-                        if (strToNum(ast[2][2][2]) == strToNum(ast[2][3][2])) then
+                if (ast[1][2] == "+") then
+                    return (strToNum(ast[2][2]) + strToNum(ast[3][2]))
+                elseif (ast[1][2] == "-") then
+                    return (strToNum(ast[2][2]) - strToNum(ast[3][2]))
+                elseif (ast[1][2] == "*") then
+                    return (strToNum(ast[2][2]) * strToNum(ast[3][2]))
+                elseif (ast[1][2] == "/") then
+                    return toInt((strToNum(ast[2][2]) / strToNum(ast[3][2])))
+                elseif (ast[1][2] == "%") then
+                    return (strToNum(ast[2][2]) % strToNum(ast[3][2]))
+                elseif (ast[1][2] == "==") then
+                        if (strToNum(ast[2][2]) == strToNum(ast[3][2])) then
                             return 1;
                         else 
                             return 0;
                         end
-                elseif (ast[2][1][2] == "!=") then
-                        if (strToNum(ast[2][2][2]) ~= strToNum(ast[2][3][2])) then
+                elseif (ast[1][2] == "!=") then
+                        if (strToNum(ast[2][2]) ~= strToNum(ast[3][2])) then
                             return 1;
                         else 
                             return 0;
                         end
-                 elseif (ast[2][1][2] == "<") then
-                        if (strToNum(ast[2][2][2]) < strToNum(ast[2][3][2])) then
+                 elseif (ast[1][2] == "<") then
+                        if (strToNum(ast[2][2]) < strToNum(ast[3][2])) then
                             return 1;
                         else 
                             return 0;
                         end
-                elseif (ast[2][1][2] == ">") then
-                        if (strToNum(ast[2][2][2]) > strToNum(ast[2][3][2])) then
+                elseif (ast[1][2] == ">") then
+                        if (strToNum(ast[2][2]) > strToNum(ast[3][2])) then
                             return 1;
                         else 
                             return 0;
                         end
-                 elseif (ast[2][1][2] == "<=") then
-                        if (strToNum(ast[2][2][2]) <= strToNum(ast[2][3][2])) then
+                 elseif (ast[1][2] == "<=") then
+                        if (strToNum(ast[2][2]) <= strToNum(ast[3][2])) then
                             return 1;
                         else 
                             return 0;
                         end
-                elseif (ast[2][1][2] == ">=") then
-                        if (strToNum(ast[2][2][2]) >= strToNum(ast[2][3][2])) then
+                elseif (ast[1][2] == ">=") then
+                        if (strToNum(ast[2][2]) >= strToNum(ast[3][2])) then
                             return 1;
                         else 
                             return 0;
@@ -255,7 +255,7 @@ function interpit.interp(ast, state, incall, outcall)
                 --outcall(numToStr(state.a[ast[5][2]]))
             elseif (type(ast[2][1]) == "table") then
                 print("CATS")
-                holder = eval_expr(ast)
+                holder = eval_expr(ast[2])
                 print(holder)
                 print("Before outcall")
                 outcall(numToStr(holder))
