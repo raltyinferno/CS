10894e657925f09c8e5b13f3f9fb05eef7aa5f31
Scott Corcoran <scott.of.alaska@gmail.com>
2016-04-11 15:27:04 -0700

    No merge conflicts please

--- interpit - TC.lua
+++ interpit - TC.lua
@@ -157,7 +157,11 @@ function interpit.interp(ast, state, incall, outcall)
 --                val1 = eval_expr(ast[2][2][2])
 --                val2 = eval_expr(ast[2][3][2])
 --                return strToNum(val1 + val2)
-                return (strToNum(ast[2][2][2]) + strToNum(ast[2][3][2]))
+                if (ast[2][1][2] == "+") then
+                    return (strToNum(ast[2][2][2]) + strToNum(ast[2][3][2]))
+                elseif (ast[2][1][2] == "-") then
+                    return (strToNum(ast[2][2][2]) + strToNum(ast[2][3][2]))
+                end
 --                              -if ast[1][2] == "t" then
 --                              -... happens
 --                              -elseiif ast[1][2] == ...  
